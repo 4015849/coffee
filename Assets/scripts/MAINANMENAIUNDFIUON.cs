cc191462ef3429f9d41d5f9c44771d1a960df9af
Abby <4015849@g.highlineschools.org>
2024-02-16 12:09:41 -0800

    partial menu functionality
    added a scripts folder and moved scripts into it. also added functionality to play and quit buttons.

--- Assets/scripts/MAINANMENAIUNDFIUON.cs
+++ Assets/scripts/MAINANMENAIUNDFIUON.cs
@@ -11,4 +11,9 @@ public class MAINANMENAIUNDFIUON : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void QuitGame()
+    {
+        Debug.Log("game hath been quitted !");
+        Application.Quit();
+    }
 }
