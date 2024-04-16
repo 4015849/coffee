c4c6035dc0f28562e78177a8ac02f3965acc5d73
Abby <4015849@g.highlineschools.org>
2024-04-16 13:34:42 -0700

    pause menu lol
    literally just copied my pause menu stuff from my breakout game but like if it works it works yk

---
+++ Assets/scripts/pause.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Continue();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
