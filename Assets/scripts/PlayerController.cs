c4639b1258d9678fc86968cc7842e0e0d0c36aa8
Abby <4015849@g.highlineschools.org>
2024-04-22 13:40:19 -0700

    some attack fixes. kind of
    attacks cancel more accurately but character doesn't really stop moving long enough for it to be obvious and also the movement lock/unlock is broken for attack/walk right. fix that tomorrow pls god

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,15 +9,15 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
-    private float attackTime = .25f;
-    private float attackCounter = .25f;
+    private float attackTime = .1f;
+    private float attackCounter = .1f;
     private bool isAttacking;
-
     Vector2 movementInput;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
@@ -50,31 +50,26 @@ public class PlayerController : MonoBehaviour
                 isAttacking = false;
             }
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("fire");
-            attackCounter = attackTime;
-            animator.SetBool("isAttacking", true);
-            isAttacking = true;
-        }
     }
 
     private void FixedUpdate()
     {
-        // If movement input is not 0, try to move
-        if (movementInput != Vector2.zero)
+        if (canMove)
         {
-           bool success =  TryMove(movementInput);
-
-            if (!success)
+            // If movement input is not 0, try to move
+            if (movementInput != Vector2.zero)
             {
-                success = TryMove(new Vector2(movementInput.x, 0));
-            }
+                bool success = TryMove(movementInput);
 
-            if (!success)
-            {
-                success = TryMove(new Vector2(0, movementInput.y));
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
             }
         }
     }
@@ -112,4 +107,24 @@ public class PlayerController : MonoBehaviour
         movementInput = movementValue.Get<Vector2>();
     }
 
+    void OnFire()
+    {
+        animator.SetTrigger("swordAttack");
+        Debug.Log("fire");
+        attackCounter = attackTime;
+        animator.SetBool("isAttacking", true);
+        isAttacking = true;
+    }
+
+    public void LockMove()
+    {
+        canMove = false;
+        Debug.Log("locked");
+    }
+
+    public void UnlockMove()
+    {
+        canMove = true;
+        Debug.Log("unlocked");
+    }
 }
